926e870f0c0ed2833622cc21fc4f99461c2463a4
Adam Wulf <adam.wulf@gmail.com>
2015-02-13 13:42:47 -0600

    initial commit

---
+++ ShapeShifter/ShapeShifter/MMDebugQuadrilateralView.h
@@ -0,0 +1,16 @@
+//
+//  MMDebugQuadrilateralView.h
+//  ShapeShifter
+//
+//  Created by Adam Wulf on 2/28/14.
+//  Copyright (c) 2014 Milestone Made, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Constants.h"
+
+@interface MMDebugQuadrilateralView : UIView
+
+-(void) setQuadrilateral:(Quadrilateral)q;
+
+@end
---
+++ ShapeShifter/ShapeShifter/MMShapeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMShapeViewController.h
+//  ShapeShifter
+//
+//  Created by Adam Wulf on 2/21/14.
+//  Copyright (c) 2014 Milestone Made, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMShapeViewController : UIViewController
+
+@end
